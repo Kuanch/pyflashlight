76a4ea69db5dc49b395ceb8796493efe291a58ce
sixigma <Sixigma6@gmail.com>
2021-05-09 01:43:19 +0800

    fix: seperate coco dataloader

--- dataset/obj_dataloader.py
+++ dataset/obj_dataloader.py
@@ -1,3 +1,5 @@
+import os
+
 import torchvision.datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
@@ -14,27 +16,60 @@ class ObjTorchLoader(object):
             transform = transforms.Compose([transforms.ToTensor(),
                                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-        coco_root = '/home/sixigma/workplace/meow/coco_data/'
-        if hasattr(torchvision.datasets, dataset_name):
-            train_set = torchvision.datasets.coco.CocoDetection(root=coco_root + 'val/val2017/',
-                                                      annFile=coco_root + 'annotations/instances_val2017.json',
-                                              transform=transform)
+        if dataset_name == 'coco':
+            self.train_loader, self.test_loader = self.__get_coco(transform,
+                                                                  train_batch_size,
+                                                                  test_batch_size,
+                                                                  collate_fn)
+        elif hasattr(torchvision.datasets, dataset_name):
+            train_set = getattr(torchvision.datasets, dataset_name)(root='./data',
+                                                                    train=True,
+                                                                    download=True,
+                                                                    transform=transform)
+            
             self.train_loader = DataLoader(train_set,
                                            batch_size=train_batch_size,
                                            shuffle=True,
                                            num_workers=1,
                                            collate_fn=collate_fn)
 
-            test_set = torchvision.datasets.coco.CocoDetection(root=coco_root + 'test/test2017/',
-                                                      annFile=coco_root + 'annotations/image_info_test2017.json',
-                                             transform=transform)
+            test_set = getattr(torchvision.datasets, dataset_name)(root='./data',
+                                                                    train=False,
+                                                                    download=True,
+                                                                    transform=transform)
+
             self.test_loader = DataLoader(test_set,
                                           batch_size=test_batch_size,
                                           shuffle=True,
                                           num_workers=1,
                                           collate_fn=collate_fn)
+            
         else:
-            raise ImportError('unknown dataset {}, only torchvision seems not to support'.format(dataset_name))
+            raise ImportError('unknown dataset {}, \
+                              only torchvision datasets are supported'.format(dataset_name))
+        
+    def __get_coco(self, transform, train_batch_size,
+                   test_batch_size, collate_fn):
+        coco_root = os.environ['COCO_ROOT']
+        train_set = torchvision.datasets.coco.CocoDetection(root=coco_root + 'val/val2017/',
+                                                            annFile=coco_root + 'annotations/instances_val2017.json',
+                                                            transform=transform)
+        train_loader = DataLoader(train_set,
+                                  batch_size=train_batch_size,
+                                  shuffle=True,
+                                  num_workers=1,
+                                  collate_fn=collate_fn)
+
+        test_set = torchvision.datasets.coco.CocoDetection(root=coco_root + 'test/test2017/',
+                                                           annFile=coco_root + 'annotations/image_info_test2017.json',
+                                                           transform=transform)
+        test_loader = DataLoader(test_set,
+                                 batch_size=test_batch_size,
+                                 shuffle=True,
+                                 num_workers=1,
+                                 collate_fn=collate_fn)
+        
+        return train_loader, test_loader
 
     def read_train(self):
         images, boxes, labels = next(iter(self.train_loader))
